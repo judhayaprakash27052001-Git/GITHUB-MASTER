af276bace8deda8164ddebfe4b3fc573b7b3d355
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2025-12-22 17:10:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log('hello');
+var search = document.getElementById('search').value;
+var results = document.getElementById('results');
+// VULNERABLE: User input is directly inserted into the DOM via innerHTML
+results.innerHTML = 'You searched for: ' + search;
