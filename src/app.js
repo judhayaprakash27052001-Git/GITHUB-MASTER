d248aad6fa10faf61177e31f8f52a4082ba67766
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2025-12-23 14:54:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// VULNERABLE CODE — FOR SECURITY TESTING ONLY
-
 function showSearch() {
   var search = document.getElementById("search").value;
   var results = document.getElementById("results");
