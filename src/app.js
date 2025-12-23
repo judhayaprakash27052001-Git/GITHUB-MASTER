9eed79e1fcbe1a45a1e1b503e8d0f282716c9b02
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2025-12-23 15:48:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
-function showSearch() {
-  var search = document.getElementById("search").value;
-  var results = document.getElementById("results");
-results.textContent = 'You searched for: ' + search;
-document.getElementById("btn").addEventListener("click", showSearch);
+const express = require('express');
+const router = express.Router()
+const request = require('request');
+
+router.post('/downlad-url', (req, res) => {
+    downloadURL(req.body.url, () =>{
+        res.send('Done')
+    }) 
+});
+
+const downloadURL = (url, onend) => {
+    const opts = {
+      uri: url,
+      method: 'GET',
+      followAllRedirects: true
+    }
+  
+    request(opts)
+      .on('data', ()=>{})
+      .on('end', () => onend())
+      .on('error', (err) => console.log(err, 'controller.url.download.error'))
+}
+
+module.exports = router
