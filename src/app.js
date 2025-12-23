b45e274e34568e1731d407f691a85185e69d4c4a
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2025-12-23 12:17:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ function showSearch() {
   var search = document.getElementById("search").value;
   var results = document.getElementById("results");
 
-  // ❌ XSS vulnerability: user input directly written to DOM
-  results.innerHTML = "You searched for: " + search;
-}
+  
 
 document.getElementById("btn").addEventListener("click", showSearch);
